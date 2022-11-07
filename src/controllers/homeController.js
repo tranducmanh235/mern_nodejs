b1fb777447c765f53d79002a6ebef6194d71df8d
Tran Duc Manh <tranducmanh235@gmail.com>
2022-11-07 15:19:39 +0700

    edit user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -33,12 +33,39 @@ let postCRUD = async (req, res) => {
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
 
-    console.log("------------------------");
-    console.log(data);
-    console.log("------------------------");
+    // console.log("------------------------");
+    // console.log(data);
+    // console.log("------------------------");
 
     return res.render("displayCRUD.ejs", {
         dataTable: data,
+    });
+};
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+
+    // console.log(userId);
+
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        // console.log("------------------------");
+        // console.log(userData);
+        return res.render("editCRUD.ejs", {
+            user: userData,
+        });
+    } else {
+        return res.send("Users not found!");
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+
+    return res.render("displayCRUD.ejs", {
+        dataTable: allUsers,
     });
 };
 
@@ -48,4 +75,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 };
